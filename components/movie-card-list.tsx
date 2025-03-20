04be72889a3992fbcc1b40cff046774ae7966bf5
ritzlog <017sjk@gmail.com>
2025-03-21 01:24:57 +0900

    feat: 영화 검색 기능 및 상세페이지 구현

--- components/movie-card-list.tsx
+++ components/movie-card-list.tsx
@@ -1,16 +1,27 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+import { searchMovies } from "actions/movieActions";
 import MovieCard from "./movie-card";
+import { Spinner } from "@material-tailwind/react";
+import { useRecoilValue } from "recoil";
+import { searchState } from "utils/recoil/atoms";
 
 export default function MovieCardList() {
+  const search = useRecoilValue(searchState);
+
+  const getAllMoviesQuery = useQuery({
+    queryKey: ["movie", search],
+    queryFn: () => searchMovies(search),
+  });
+
   return (
     <div className="grid gap-1 md:grid-cols-4 gird-cols-3 w-full h-full">
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
+      {getAllMoviesQuery.isLoading && <Spinner />}
+      {getAllMoviesQuery.data &&
+        getAllMoviesQuery.data.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
     </div>
   );
 }
